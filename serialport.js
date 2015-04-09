b008ebe93d076e1bfa9f555e46e4649f37645bd0
shakti <mbshakti@gmail.com>
2015-04-09 15:59:26 -0400

    made more changes

--- serialport.js
+++ serialport.js
@@ -1,5 +1,5 @@
 var SerialPort = require("serialport").SerialPort
-var serialPort = new SerialPort("/dev/cu.usbmodem1421", {
+var serialPort = new SerialPort("/dev/cu.usbmodem1411", {
   baudrate: 57600
 });
 var servi = require('servi');
@@ -30,16 +30,24 @@ function showError(error) {
 }
 
 function saveLatestData(data) {
-   console.log(data.toString())
+console.log(data.toString())
+     var rawData = data.toString();
+     if(rawData.indexOf("B") == 0){
+//this string starts with B, we got heartrate BPM
+var heartRate = rawData.split("B")[1];
+console.log("got heartRate = "+ heartRate);
+}
+if(rawData.indexOf("Q") == 0){
+//this string starts with Q, we got IBI
+var ibi = rawData.split("Q")[1];
+console.log("got ibi = "+ ibi);
+}   
    latestData = data;
 }
 
 function sendData(request) {
   // print out the fact that a client HTTP request came in to the server:
-  console.log("Got a client request, sending them the data.");
+  // console.log("Got a client request, sending them the data.");
   // respond to the client request with the latest serial string:
   request.respond(latestData);
 }
-
-
-
